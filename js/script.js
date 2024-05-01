7bcfdc2f4656bff6c5005e66bb569d3d814ed9fc
LANKOANDE Urbain <102440329+Lankoande-Urbain@users.noreply.github.com>
2024-05-01 20:57:30 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -70,12 +70,22 @@ ScrollReveal({
   delay: 200,
 });
 
+ScrollReveal2({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
 ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
 ScrollReveal().reveal(
   ".home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form",
   { origin: "bottom" }
 );
 ScrollReveal().reveal(".home-content h1, .about-img img", { origin: "left" });
+
+ScrollReveal2().reveal(".about-img img", { origin: "left" });
+
 
 crollReveal().reveal(".home-content h3, .home-content p, .about-content", {
   origin: "right",
