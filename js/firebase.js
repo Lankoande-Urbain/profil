a034797384781c975e30007a4660044f4d78bc46
lankoandeurbain8 <lankoandeurbain8@gmail.com>
2024-05-10 15:48:28 +0000

    Test 4

--- js/firebase.js
+++ js/firebase.js
@@ -1,27 +1,18 @@
 // import firebase from './firebase';
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
     apiKey: "AIzaSyBNAJvJLaqsRNWBavqvaSh43l0sPVHTacM",
     authDomain: "portfolio-f1e77.firebaseapp.com",
     databaseURL: "https://portfolio-f1e77-default-rtdb.firebaseio.com",
     projectId: "portfolio-f1e77",
     storageBucket: "portfolio-f1e77.appspot.com",
     messagingSenderId: "761853165145",
-    appId: "1:761853165145:web:28be50129423794ef37b48",
-    measurementId: "G-XH21CKZHX8"
+    appId: "1:761853165145:web:ecd1c2dca567e617f37b48",
+    measurementId: "G-FTLNESRR6K"
   };
+  firebase.initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
 
 document.getElementById('contact-form').addEventListener('submit', function(e) {
 
@@ -37,7 +28,6 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
   
     // Enregistrez les données dans Firebase
     var db = firebase.database();
-    console.log('send data')
     db.ref('WebMessage').push({
       name: name,
       email: email,
